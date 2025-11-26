b0c2c22463e21b41cf8377b9b33e903eaad4df45
NDNEX <9hm@naver.com>
2025-11-26 22:52:44 +0900

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -86,7 +86,7 @@ export const Home: React.FC = () => {
           <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] opacity-10 mix-blend-overlay"></div>
         </div>
         
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full relative z-10 pt-20 pb-32">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full relative z-10 pt-28 md:pt-20 pb-32">
           <div className="max-w-5xl">
             <div className="inline-flex items-center gap-3 px-4 py-2 rounded-full bg-white/5 border border-white/10 backdrop-blur-md mb-8 animate-fade-in shadow-2xl">
               <span className="flex h-2 w-2 relative">
