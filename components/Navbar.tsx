b9c4cf51f5954fbfbffd7ae8be635ae1d10ed6ea
NDNEX <9hm@naver.com>
2025-11-26 19:28:38 +0900

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar: React.FC = () => {
   // Custom Image Logo Component
   const NdnexLogo = ({ className }: { className?: string }) => (
     <img 
-      src=https://postfiles.pstatic.net/MjAyNTExMjZfMTUz/MDAxNzY0MTUyNzE4ODc0.tN_9KMTMDbY6zKr7nOcYgsjing7i16VmN6qapQv1Hucg.pyE3cIovCIO2ODSAfky-Bc6pHH21uFOo-luNUcXKB-Yg.PNG/logo-removebg-preview.png?type=w773"
+      src="https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20170711_154%2F14997399705755qbhS_PNG%2F186661515629818_0.png"
       alt="NDNEX Logo"
       className={`${className} rounded-md`}
     />
